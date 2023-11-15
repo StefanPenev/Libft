0c540010d0850ddfcd724ed3c66626df11e5a1d2
StefanPenev <stfnpenev@gmail.com>
2023-11-16 00:41:06 +0100

    bonus functions

---
+++ extra/ft_lstclear.c
@@ -0,0 +1,9 @@
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	if (!lst || !del || !(*lst))
+		return ;
+	ft_lstclear(&(*lst)->next, del);
+	(del)((*lst)->content);
+	free(*lst);
+	*lst = NULL;
+}
